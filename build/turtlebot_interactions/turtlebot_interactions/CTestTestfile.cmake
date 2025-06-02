b89fbb350567660047099a646029b5fcf8203716
Banzile <125497238+nhlebelabz74@users.noreply.github.com>
2025-06-02 02:33:22 +0200

    Delete build directory

--- build/turtlebot_interactions/turtlebot_interactions/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/eliznab74/ros_home/robot_assignment_ws/src/turtlebot_interactions/turtlebot_interactions
-# Build directory: /home/eliznab74/ros_home/robot_assignment_ws/build/turtlebot_interactions/turtlebot_interactions
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
